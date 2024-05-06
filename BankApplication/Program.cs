085643304616e667e78e05cf66c31bb8f787fddf
MFSeng <Mitchellseng02@gmail.com>
2024-05-06 21:12:28 +0100

    Edited SRS
    added a new SRS and more tables to the database. connection to the forms is still needed

--- BankApplication/Program.cs
+++ BankApplication/Program.cs
@@ -16,7 +16,7 @@ namespace BankApplication
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginPage());
         }
     }
 }
